24e5cb422a46996fa829624e22292aa048b4dfd2
pjc1991 <whdekf@gmail.com>
2023-06-24 23:45:17 +0900

    feat : implement basic user api

--- common/views.py
+++ common/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+
+from common.serializers import UserSerializer
+
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
