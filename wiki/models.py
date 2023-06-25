2b590183a26551ed375e251a868e1f5a00d1a4c2
pjc1991 <whdekf@gmail.com>
2023-06-25 21:07:56 +0900

    feat : add Universe model.

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Universe(models.Model):
+    """
+    A universe is a collection of Objects, which can be edited by allowed users.
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey('auth.User', related_name='owned_universes', on_delete=models.CASCADE)
+    allowed_users = models.ManyToManyField('auth.User', related_name='allowed_universes')
+
+    def __str__(self):
+        return self.name
+
