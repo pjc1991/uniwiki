29b8f665504b7687fd60991965d119bf514624e2
pjc1991 <whdekf@gmail.com>
2023-07-13 01:37:12 +0900

    refactor : now both serializers model serializer

--- wiki/serializers.py
+++ wiki/serializers.py
@@ -1,28 +1,43 @@
 from django.contrib.auth.models import User
-from django.db import models
 from rest_framework import serializers
 
 from wiki.models import Universe, WikiDocument
 
 
-class WikiDocsSerializer(serializers.HyperlinkedModelSerializer):
-    universe = models.ForeignKey(Universe, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    related_documents = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    document_type = serializers.ChoiceField(choices=WikiDocument.DocumentType.choices)
+class WikiDocsSerializer(serializers.ModelSerializer):
+    universe_name = serializers.CharField(source='universe.name', read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
-    # version should be read-only
-    # is_published should be read-only
     class Meta:
         model = WikiDocument
-        fields = ['id', 'name', 'description', 'universe', 'owner', 'related_documents', 'document_type', 'version',
-                  'created_at', 'modified_at', 'is_published', 'is_deleted']
+        fields = [
+            'id',
+            'name',
+            'description',
+            'universe_name',
+            'owner',
+            'version',
+            'related_documents',
+            'document_type',
+            'created_at',
+            'modified_at',
+            'is_published',
+            'is_deleted',
+        ]
 
 
 class UniverseSerializer(serializers.ModelSerializer):
-    wiki_docs = WikiDocsSerializer(many=True, read_only=True)
+    wiki_document = WikiDocsSerializer(many=True, read_only=True)
 
     class Meta:
         model = Universe
-        # all field
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'description',
+            'created_at',
+            'modified_at',
+            'owner',
+            'allowed_users',
+            'wiki_document',
+        ]
