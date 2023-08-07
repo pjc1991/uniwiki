05d8567ee54b4b5b49268741d4f6e06a9178d654
JC Park <whdekf@naver.com>
2023-08-07 21:22:12 +0900

    feat : add base model and customized user

--- common/models.py
+++ common/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    """
+    Base model for all models
+    """
+    # Primary key, auto increment
+    id = models.AutoField(primary_key=True, db_index=True, help_text="Primary key")
+
+    # Created time
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Created time")
+
+    # Updated time
+    updated_at = models.DateTimeField(auto_now=True, help_text="Updated time")
+
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        """
+        admin page will show the object's id
+        if there is no __str__ method override
+        """
+        return str(self.id)
+
+
+class UniUser(AbstractUser):
+    """
+    Custom user model
+    """
+
+    # There is nothing to customize for now
+    # However, we can add more fields here when needed
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
