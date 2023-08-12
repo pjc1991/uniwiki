86cf262e853c90b12f1afbdfe6ab400801fa45fe
JC Park <whdekf@naver.com>
2023-08-12 22:20:18 +0900

    chore : add static directory

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -171,6 +174,4 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-AUTH_USER_MODEL = 'common.UniUser'
 
