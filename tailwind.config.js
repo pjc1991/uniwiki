78e8d2a2e7d22238a9fcadd4d9c69f06d3bdecbb
pjc1991 <whdekf@gmail.com>
2023-08-14 16:52:56 +0900

    chore : configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./static/**/*.{html,js}",
+      "./templates/**/*.html",
+  ],
   theme: {
     extend: {},
   },
