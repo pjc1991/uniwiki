e2fcce8445f9475e6b46c419a2a0523c29899b35
JC Park <whdekf@naver.com>
2023-08-16 19:26:16 +0900

    fix : missing imports

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path, include
+
+from wiki import views
+
 urlpatterns = [
     path('', views.UniWikiListView.as_view(), name='uniwiki-list'),
     path('<int:pk>/', views.UniWikiDetailView.as_view(), name='uniwiki-detail'),
