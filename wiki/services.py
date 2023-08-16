dde630ce503c892d37ab3135f004f5bbd2f1b6c7
JC Park <whdekf@naver.com>
2023-08-16 19:08:17 +0900

    feat : add login user service

--- wiki/services.py
+++ wiki/services.py
@@ -1,6 +1,9 @@
 # Create your services here.
-from wiki.models import Universe
+from django.contrib.auth import authenticate, login
+
+from common.forms import UniUserLoginForm
 from common.models import UniUser
+from wiki.models import Universe
 
 
 # check if user is available for the universe or card
@@ -25,5 +28,26 @@ def is_available_universe(user: UniUser, universe: Universe):
     if universe.allowed_users.filter(id=user.id).exists():
         return True
 
+    return False
 
-    return  False
+
+def auth_login(request, user):
+    pass
+
+
+def login_user(request, *args, **kwargs) -> bool:
+    form = UniUserLoginForm(request.POST)
+    if not form.is_valid():
+        print("Form is not valid"
+              "form.errors: ", form.errors)
+        raise Exception("Form is not valid")
+
+    username = form.cleaned_data.get('username')
+    password = form.cleaned_data.get('password1')
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        raise Exception("User is None")
+
+    login(request, user)
+    return True
