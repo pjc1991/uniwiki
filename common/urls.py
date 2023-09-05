da4ba2683ec72d7789c960d906690eb95e4e120c
JC Park <whdekf@naver.com>
2023-09-05 22:43:30 +0900

    REFACTOR : add rest auth

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from common import views
 
 urlpatterns = [
     path('login/', views.UniWikiLoginView.as_view(), name='login'),
+    path('api/auth/', include('dj_rest_auth.urls')),
 ]
