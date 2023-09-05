0d1457a65df7673c9e2143073a9afc130b8232e6
JC Park <whdekf@naver.com>
2023-09-05 22:43:57 +0900

    REFACTOR : simplify to structure

--- static/script/script.js
+++ static/script/script.js
@@ -2,12 +2,12 @@ Vue.component('template-form', {
     props: ['field'], template: `
 <div>
     <div>
-        <label v-bind:id="field.id" class="block font-medium text-gray-700" v-bind:text="field.name">
+        <label :id="field.id" class="block font-medium text-gray-700" d:text="field.name">
             
         </label>
     </div>
     <div class="">
-        <input type="text" v-bind:id="field.id" v-bind:name="field.name" v-bind:placeholder="field.placeholder"
+        <input type="text" :id="field.id" :name="field.name" :placeholder="field.placeholder"
                class="w-full p-2 rounded border focus:border-blue-400 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
     </div>
 </div>
@@ -22,17 +22,29 @@ Vue.component('template-button', {
 `
 })
 
+Vue.component('login-form', {
+    props: ['loginForm'],
+    template: `
+<div id="login-form" class="bg-gray-100 p-8 rounded shadow-md w-1/3">
+    <template-form v-for="field in loginForm[fields]" :field="field" v-bind:key="field.id" ></template-form>
+    <template-button v-bind:button="loginForm.loginButton"></template-button>
+</div>
+   `,
+    components: {
+        'template-form': 'template-form',
+        'template-button': 'template-button'
+    }
+});
+
 const login = new Vue({
     el: '#login',
     data: {
-            fields: [{id: 'username', name: 'username', placeholder: 'Email'}, {
-                id: 'password',
-                name: 'password',
-                placeholder: 'Password'
-            }],
-            loginButton: {
-                name: 'Login'
-            }
+        loginInfo: {
+            fields: [
+                {id: 'username', name: 'username', placeholder: 'Email'},
+                {id: 'password', name: 'password', placeholder: 'Password'}],
+            loginButton: {name: 'Login'}
+        }
     },
 });
 
