44e4cbd5e241412269492e6d9d04dbf3bd3312ec
JC Park <whdekf@naver.com>
2023-09-08 23:47:00 +0900

    CHORE : Basic setup for django and vue integration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const BundleTracker = require('webpack-bundle-tracker')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+    outputDir: '../static',
+    assetsDir: 'assets',
+    indexPath: '../templates/wiki/app.html',
+    lintOnSave: true,
+    runtimeCompiler: true,
+    configureWebpack: {},
+    chainWebpack: config => {
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{filename: '../webpack-stats.json'}])
+            .tap(args => {
+                return [{
+                    path: __dirname, filename: 'webpack-stats.json'
+                }]
+            })
+    },
 })
