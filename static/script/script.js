108554b0d91bfbeeb11f7358f7eba4c0270b1d0b
JC Park <whdekf@naver.com>
2023-09-08 23:19:42 +0900

    CHORE : recreated vue project

--- static/script/script.js
+++ static/script/script.js
@@ -1,23 +1,23 @@
 Vue.component('template-form', {
     props: ['field'], template: `
-<div>
-    <div>
+<div class="">
+    <div >
         <label :id="field.id" class="block font-medium text-gray-700" d:text="field.name">
-            
         </label>
     </div>
     <div class="">
         <input type="text" :id="field.id" :name="field.name" :placeholder="field.placeholder"
-               class="w-full p-2 rounded border focus:border-blue-400 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+               class="w-full  p-2 rounded border focus:border-blue-400 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
     </div>
 </div>
     `
 })
 
 Vue.component('template-button', {
-    props: ['button'], template: `
-<div>
-    <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">{{ button.name }}</button>
+    props: ['button'],
+    template: `
+<div class="grow">
+    <button type="submit" v-bind:class="button.color" :id="button.id" class="w-full text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">{{ button.name }}</button>
 </div>
 `
 })
@@ -29,7 +29,10 @@ Vue.component('login-form', {
     template: `
 <div id="login-form" class="bg-gray-100 p-8 rounded shadow-md w-1/3">
     <template-form v-for="field in loginFormProps.fields" :field="field" v-bind:key="field.id" ></template-form>
-    <template-button v-bind:button="loginFormProps.loginButton" @click="alert('test')"></template-button>
+    <div class="">
+        <template-button :button="loginFormProps.buttons[0]" @click="alert('test')"></template-button>
+        <template-button :button="loginFormProps.buttons[1]" @click="alert('test')"></template-button>
+    </div>
 </div>
    `,
 });
@@ -43,8 +46,12 @@ const app = new Vue({
             fields: [
                 {id: 'username', name: 'username', placeholder: 'Email'},
                 {id: 'password', name: 'password', placeholder: 'Password'}],
-            loginButton: {name: 'Login'}
+            buttons: [
+                {id: 'login', name: 'login', placeholder: 'Login', color: 'bg-blue'},
+                {id: 'signup', name: 'signup', placeholder: 'Sign Up', color: 'bg-green'}]
         }
     },
 });
+
+
 
