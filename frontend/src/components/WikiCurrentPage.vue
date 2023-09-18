5ce82ceb0c49fa58d5010158e67e8002e29d3c84
JC Park <whdekf@naver.com>
2023-09-18 22:23:12 +0900

    FEAT : basic implement of creating page

--- frontend/src/components/WikiCurrentPage.vue
+++ frontend/src/components/WikiCurrentPage.vue
@@ -6,8 +6,44 @@ export default defineComponent({
   props: [
       'page_id'
   ],
+  data() {
+    return {
+      wiki_content: ''
+    }
+  },
   methods: {
     getWikiContent(page_id) {
+    },
+    inputHandler() {
+      // get the content of the textarea
+      const content = this.wiki_content
+
+      // the first line is always the title and start with #
+      // get the data with the first line
+      const name = content.split('\n')[0].replace('#', '').trim()
+      const description = content.replace(title, '').trim()
+
+      // send the data to the backend
+      const csrftoken = getCookie('csrftoken')
+      fetch('/api/wiki/wikidocs/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({
+          name: name,
+          description: description
+        })
+      }).then(response => {
+        if (response.status !== 200) {
+          throw new Error('save failed')
+        }
+      }).then(data => {
+        console.log(data)
+      }).catch((error) => {
+        console.log(error)
+      });
     }
   }
 })
@@ -15,9 +51,9 @@ export default defineComponent({
 
 <template>
   <div class="flex flex-col w-3/4">
-    <textarea
+    <textarea name="wiki-content" id="wiki-content" cols="30" rows="10" v-model="wiki_content" @input="inputHandler"
         class="w-full h-full border border-gray-200 rounded-lg px-4 py-2 bg-gray-200 resize-none focus:outline-none font-serif overscroll-auto"
-    >This is wiki contents.</textarea>
+    ># New Page</textarea>
   </div>
 </template>
 
